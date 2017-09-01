84206dec76300d7892d6f9cc4fef84ef759a290e
Edgar Jan <krodanix@gmail.com>
2017-09-01 14:19:54 +0300

    small fixes

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,14 +2,13 @@ import _ from 'lodash';
 
 export class MainController {
 
-  constructor($scope, sessionData, $state, $transitions, moment, $timeout, $interval) {
+  constructor($scope, sessionData, $state, $transitions, moment, $interval) {
     'ngInject';
 
     this.currentSesstionId = $state.params.sessionId;
     this.$state = $state;
     this.moment = moment;
     this.$scope = $scope;
-    this.$timeout = $timeout;
 
     $transitions.onSuccess({}, transition => {
 
@@ -62,7 +61,7 @@ export class MainController {
   sessionClass (session) {
     return session && {
       removed: session.destroyed,
-      inactive: _.get(session, 'lastStatus.url') === 'applicationDidEnterBackground',
+      inactive: _.get(session, 'lastStatus.url') === 'applicationDidEnterBackground' || _.get(session, 'lastStatus.url') === 'UIApplicationStateBackground',
       active: session.id === this.currentSesstionId
     };
   }
